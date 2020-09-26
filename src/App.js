971e2d2e83fb2fcac93441cb7bc3235b1299f3d4
zzhalex <zzh001xx@hotmail.com>
2020-09-25 23:32:24 -0700

    website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/header";
+import Body from "./components/body";
+
+import textInfo from "./textInfo";
+
+let headerInfo = textInfo.headerinfo;
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { navItems: headerInfo, activeId: 1 };
+  }
+
+  setHeader = (data) => {
+    console.log("APP");
+    console.log(data);
+    this.setState({
+      activeId:data
+    })
+  };
+  render() {
+    let Nav = this.state.navItems
+    let activeId = this.state.activeId
+    return (
+      <div className="App">
+        <Header numbers={Nav} activeId={activeId} setHeader={this.setHeader} />
+        <Body activeId = {activeId} />
+      </div>
+    );
+  }
 }
 
 export default App;
